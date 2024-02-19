c70d579fc687b5f3e150fed6e6363a9d41298368
shpetimsedolli <shpeti101@gmail.com>
2024-02-19 09:37:53 +0100

    added ceo section and brands

--- src/components/Brands/Brands.jsx
+++ src/components/Brands/Brands.jsx
@@ -1,8 +1,32 @@
 import React from "react";
 import "./brands.css";
+import group39 from "../../assets/Group 39.svg";
+import xp7 from "../../assets/XP7E6wK2.svg";
+import yves from "../../assets/yves-rocher-logo.svg";
+import aa from "../../assets/aa.svg";
+import layer from "../../assets/Layer 0.svg";
+import dd from "../../assets/2dde905a2bf4793361d4d7dc4cdf3db3.svg";
+import ed from "../../assets/1ed2649ba3bc113b76f9ee30f24be572.svg";
 
 const Brands = () => {
-  return <div>Brands</div>;
+  return (
+    <div className="brands">
+      <div className="brandswrap">
+        <h5>
+          We are part of a initiative of the top educators around the word
+        </h5>
+        <div className="brands__logos">
+          <img src={group39} alt="" />
+          <img src={xp7} alt="" />
+          <img src={yves} alt="" />
+          <img src={aa} alt="" />
+          <img src={layer} alt="" />
+          <img src={dd} alt="" />
+          <img src={ed} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Brands;
--- src/components/Ceo/Ceo.jsx
+++ src/components/Ceo/Ceo.jsx
@@ -1,8 +1,30 @@
 import React from "react";
 import "./ceo.css";
+import andrea from "../../assets/pexels-andrea-piacquadio-832998.svg";
 
 const Ceo = () => {
-  return <div>Ceo</div>;
+  return (
+    <div className="ceo">
+      <div className="container">
+        <div className="ceowrap">
+          <img className="academy__img" src={andrea} alt="" />
+          <div className="ceo__content">
+            <p>
+              Language Kos helped me learn you are responsible for you own
+              future. I was about to travel for work in France but I didn't know
+              their language, thanks to them I learned it all, I am very
+              grateful.
+            </p>
+            <span>JIM KENNEDY - CEO & FOUNDER KEYS</span>
+          </div>
+        </div>
+        <h6>
+          Are you ready to learn new languages and <br />
+          explore new cultures ?
+        </h6>
+      </div>
+    </div>
+  );
 };
 
 export default Ceo;
